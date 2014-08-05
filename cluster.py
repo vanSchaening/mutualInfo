5da0e1a229bc95c18b6b54df618a58a7323535ed
vanSchaening <c.schaening@gmail.com>
2014-08-05 19:57:52 +0000

    Mask missing values

--- cluster.py
+++ cluster.py
@@ -6,8 +6,17 @@ import sys
 if len(sys.argv) < 3:
     exit("Usage:\n\tcluster.py <infile> <out_name>\nWhere <out_name> is a name for output files without an extension.\n")
 
+# Open file into record object
 f = open(sys.argv[1])
 record = read(f)
+
+# Apply a mask to missing values
+def maskValue(v):
+    if v == 0.0:
+        return 0
+    return 1
+record.mask = np.array([ map(maskValue, row)
+                         for row in record.data ])
 
 # Hierarchical clustering
 interactions_tree = record.treecluster(method='m',transpose=0)
